6b41d3a3ce2c50e9f51b05d5fd47d048826ca16c
Adam Paul <adumb42@gmail.com>
2021-04-02 15:04:17 +0100

    sixth commit

--- src/components/holidays/HolidayHome.js
+++ src/components/holidays/HolidayHome.js
@@ -216,10 +216,7 @@ class HolidayList extends React.Component {
     render() {
         return (
             <div className="search-bar ui segment">
-                <div className="field"
-                style={{
-                    "zIndex": 8
-                }}>
+                <div className="field">
                 <DateRangePicker
                         startDate={this.state.startDate} // momentPropTypes.momentObj or null,
                         startDateId="your_unique_start_date_id" // PropTypes.string.isRequired,
